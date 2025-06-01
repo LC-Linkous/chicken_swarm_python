7946a6169f227b96aec67f8136d7e819d07f906a
L. Linkous <LC-Linkous@users.noreply.github.com>
2025-06-01 16:03:41 -0400

    maxed() inequality update

--- src/chicken_swarm.py
+++ src/chicken_swarm.py
@@ -620,7 +620,7 @@ class swarm:
         return convergence
     
     def maxed(self):
-        max_iter = self.iter > self.maxit
+        max_iter = self.iter >= self.maxit
         return max_iter
     
     def complete(self):
